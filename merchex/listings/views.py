bb7a4e43305a57f96fc029fade9050073b2a02f4
Nicolas GOASGUEN <n.goasguen@gmail.com>
2024-10-18 16:05:40 +0200

    add dynamic/static templates for listings, about-us and contact-us views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -8,19 +8,11 @@ def hello(request):
     return render(request, 'listings/hello.html', {'bands': bands})
 
 def listings(request):
-    listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Voici quelques exemples d'annonces :<p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-        </ul>
-    """)
+    listings_ = Listing.objects.all()
+    return render(request, 'listings/listings.html', {'listings': listings_})
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Site créé par Nicolas GOASGUEN.</p>')
+    return render(request, 'listings/about.html')
 
 def contact(request):
-    return HttpResponse('<h1>Contactez-nous</h1> <p>Informations à venir.</p>')
+    return render(request, 'listings/contact.html')
